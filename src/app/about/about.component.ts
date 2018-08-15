49bf6cbcbdf832a226b8d4e0d5c91aa9d845f056
Blaise Schaeffer <Blaise.Schaeffer1@ibm.com>
2018-08-15 07:12:15 -0400

    faet(about): wi 2 - Added skills in the form of images

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Image } from '../models/image.model';
 
 @Component({
   selector: 'app-about',
@@ -6,6 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
+  public skillsImages = [
+    new Image('/assets/images/angular.png', 'angular'),
+    new Image('/assets/images/ts.png', 'typescript'),
+    new Image('/assets/images/js.png', 'javasctipt'),
+    new Image('/assets/images/d3.png', 'd3'),
+    new Image('/assets/images/html.png', 'html'),
+    new Image('/assets/images/css.png', 'css'),
+    new Image('/assets/images/sass.png', 'sass'),
+    new Image('/assets/images/vscode.png', 'vscode'),
+    new Image('/assets/images/github.png', 'github'),
+    new Image('/assets/images/travis.png', 'travis'),
+    new Image('/assets/images/terminal.png', 'terminal'),
+    new Image('/assets/images/webpack.png', 'webpack'),
+    new Image('/assets/images/nodejs.png', 'nodejs'),
+    new Image('/assets/images/npm.png', 'npm'),
+    new Image('/assets/images/rest.png', 'rest'),
+    new Image('/assets/images/ubuntu.svg', 'ubuntu'),
+    new Image('/assets/images/react.png', 'react'),
+    new Image('/assets/images/redux.png', 'redux'),
+    new Image('/assets/images/slack.png', 'slack'),
+    new Image('/assets/images/java.png', 'java'),
+    new Image('/assets/images/sql.png', 'sql'),
+    new Image('/assets/images/db2.png', 'db2'),
+    new Image('/assets/images/wordpress.png', 'wordpress')
+  ];
 
   constructor() { }
 
---
+++ src/app/models/image.model.ts
@@ -0,0 +1,6 @@
+export class Image {
+    constructor(
+        public src: string,
+        public altText: string
+    ) { }
+}
